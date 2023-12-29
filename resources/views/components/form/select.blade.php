56cf1b3d49492fd4b0e1758a2ef3af6afb70caa1
IvanTunelab <Ivan.l@tunelab.net>
2023-12-29 11:08:52 +0200

    Fixed select field

--- resources/views/components/form/select.blade.php
+++ resources/views/components/form/select.blade.php
@@ -13,28 +13,9 @@
             targetValue: "{{$modalValue}}",
             options: {!! $options !!},
             placeholder: "{{$placeholder}}",
-            onSelected(select, option) {
+            onSelected(select) {
                 document.querySelector("[name='{{$name}}']").value = select.value;
-                // выбранное значение
-                console.log(`Выбранное значение: ${select.value}`);
-                // индекс выбранной опции
-                console.log(`Индекс выбранной опции: ${select.selectedIndex}`);
-                // выбранный текст опции
-                const text = option ? option.textContent : '';
-                console.log(`Выбранный текст опции: ${text}`);
             },
-        });
-
-        document.querySelector('.itc-select').addEventListener('itc.select.change', (e) => {
-            const btn = e.target.querySelector('.itc-select__toggle');
-            // выбранное значение
-            console.log(`Выбранное значение: ${btn.value}`);
-            // индекс выбранной опции
-            console.log(`Индекс выбранной опции: ${btn.dataset.index}`);
-            // выбранный текст опции
-            const selected = e.target.querySelector('.itc-select__option_selected');
-            const text = selected ? selected.textContent : '';
-            console.log(`Выбранный текст опции: ${text}`);
         });
     })
 
